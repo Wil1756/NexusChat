d0c4c13f194496561ed59a5c9d14f3c59e37be7f
John William <johnwilliams1756@gmail.com>
2025-03-22 23:27:47 +0300

    add llm file

---
+++ src/llm.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from src.prompt import system_instruction
+
+client = OpenAI()
+
+messages = [
+    {"role": "system", "content": system_instruction},
+]
+def get_response(messages, model="gpt-4o-mini", temperature= 0):
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature
+    )
+    return response.choices[0].message.content
